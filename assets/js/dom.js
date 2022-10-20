9a2e3a209b5a15c69af0e6c1ce58d76311cbc73d
Volodymyr Sokolov <sokolmx@gmail.com>
2022-10-20 14:59:05 +0300

    init

---
+++ assets/js/dom.js
@@ -0,0 +1,122 @@
+export let moviesList = null;
+export let inputSearch = null;
+export let triggerMode = false;
+
+export const createElement = ({type, attrs, container = null, position = 'append', evt = null, handler = null}) => {
+  const el = document.createElement(type);
+  
+  Object.keys(attrs).forEach((key) => {
+    if (key !== 'innerHTML') el.setAttribute(key, attrs[key])
+    else el.innerHTML = attrs[key];
+  });
+
+  if (container && position === 'append') container.append(el)
+  if (container && position === 'prepend') container.prepend(el)
+  if (evt && handler && typeof handler === 'function') el.addEventListener(evt, handler);
+
+  return el;
+};
+
+export const createMarkup = () => {
+  const container = createElement({
+   type: 'div',
+   attrs: {class: 'container'},
+   container: document.body,
+   position: 'prepend'
+  });
+
+  createElement({
+    type: 'h1', 
+    attrs: {innerHTML : 'Приложение для поиска фильмов'},
+    container});
+    
+  const searchBox = createElement({
+    type: 'div', 
+    attrs: {class: 'search'},
+    container
+  });
+  
+  const inputBox = createElement({
+    type: 'div',
+    attrs: {
+      class: 'search__group search__group--input'
+    },
+    container: searchBox
+  });
+
+  createElement({
+    type: 'label',
+    attrs: {
+      class: 'search__input-label',
+      for: 'search',
+      innerHTML: 'Поиск фильмов'
+    },
+    container: inputBox
+  });
+
+  inputSearch = createElement({
+    type: 'input', 
+    attrs: {class: 'search__input', 
+    id: 'search',
+    type: 'search',
+    placeholder: 'Начните вводить текст'},
+    container: inputBox
+  });
+
+  const checkBox = createElement({
+    type: 'div',
+    attrs: {
+      class: 'search__group search__group--checkbox'
+    },
+    container: searchBox
+  });
+
+  createElement({
+    type: 'input',
+    attrs: {
+      class: 'search__checkbox',
+      id: 'checkbox',
+      type: 'checkbox'
+    },
+    container: checkBox,
+    evt: 'click',
+    handler: () => triggerMode = !triggerMode
+  });
+  
+  createElement({
+    type: 'label',
+    attrs: {
+      class: 'search__checkbox-label',
+      for: 'checkbox',
+      innerHTML: 'Добавлять фильмы к уже найденным'
+    },
+    container: checkBox
+  });
+
+  moviesList  = createElement({
+    type: 'div',
+    attrs: {class: 'movies'},
+    container 
+  });
+};
+
+export const addMovieToList = (movie) => {
+  const item = createElement({
+    type: 'div',
+    attrs: {class: 'movie'},
+    container: moviesList
+  });
+
+  createElement({
+    type: 'img',
+    attrs: {
+      class: 'movie__image',
+      src: /(http|https):\/\//i.test(movie.Poster) ? movie.Poster : 'assets/img/No-Image-Placeholder.png',
+      alt: `${movie.Title}, ${movie.Year}`,
+      title: `${movie.Title}, ${movie.Year}`
+    },
+    container: item
+  });
+};
+
+export const clearMoviesMarkup = (el) => el && (el.innerHTML = '');
---
+++ assets/js/movies.js
@@ -0,0 +1,47 @@
+import {
+  createMarkup,
+  addMovieToList, 
+  clearMoviesMarkup, 
+  inputSearch, 
+  moviesList, 
+  triggerMode
+} from './dom.js';
+
+let searchLast = null;
+let apiURL = null;
+
+const API_KEY = '9aa0dabb';
+
+const getData = (url) => fetch(url)
+.then((response) => response.json())
+.then((json) => json.Search);
+
+const debounce = (() => {
+  let timer = null;
+  return (cb, ms) => {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+  }
+    timer = setTimeout(cb, ms);
+  };
+})();
+
+export const inputSearchHandler = (e) => {
+  debounce(() => {
+    const searchString = e.target.value.trim();
+    if (searchString && searchString.length > 2 && searchString !== searchLast) {
+      if (!triggerMode) clearMoviesMarkup(moviesList);
+      getData(`${apiURL}/?apikey=${API_KEY}&s=${searchString}`)
+      .then((movies) => movies.forEach(movie => addMovieToList(movie)))
+      .catch((err) => console.log(err));
+    }
+    searchLast = searchString;
+  }, 2000);
+};
+
+export const appInit = (url) => {
+  createMarkup();
+  apiURL = url || 'http://www.omdbapi.com'
+  inputSearch.addEventListener('keyup', inputSearchHandler); 
+};
